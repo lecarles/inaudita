83e463fdb010c94935a8dc6ebcb7445755bd8be2
lecarles <carles2612@gmail.com>
2025-11-04 15:28:27 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
         <a href="mailto:info@inauditafilms.com">info@inauditafilms.com</a>
         <a href="https://youtube.com/" target="_blank" rel="noopener">YOUTUBE</a>
       </div>
-      <div class="text-base p-(--md) uppercase leading-[0.9]">
+      <div class="text-base p-(--md) uppercase leading-[0.9] max-w-[70ch]">
         Hay una electricidad especial que nos recorre cuando escuchamos y vemos al público reaccionar por primera vez ante las obras que les presentamos. Necesitamos sentir esa sensación de placer y alivio como forma de relacionarnos con el mundo.
       </div>
       <inaudita-logo class="mt-auto"/>
